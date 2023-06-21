925605638d5fe961c4d918c537cd2dcea6ffc8eb
Daniel <dany97971@gmail.com>
2023-06-21 17:57:59 +0200

    Delete useless routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +9,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
