e790b7d10c8c8c241ecd1e2228504d2bb53b42a9
Daniel <dany97971@gmail.com>
2023-06-24 22:33:00 +0200

    downgrade UserResource to laravel 9

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'id',
+        'updated_at',
+        'created_at',
+        'email_verified_at'
     ];
 
     /**
